c6a21e95acf9bbee2eab84d2ba3115e89691ae56
Tres Seaver <tseaver@palladion.com>
2006-03-13 22:07:24 +0000

    Package names can't embed dots.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ try:
 except ImportError, e:
     from distutils.core import setup, Extension
 
-setup(name='zope.exceptions',
+setup(name='zope_exceptions',
       version='3.0',
       url='http://svn.zope.org/zope.exceptions',
       license='ZPL 2.1',
@@ -37,8 +37,8 @@ setup(name='zope.exceptions',
 
       namespace_packages=['zope',],
       tests_require = ['zope.testing'],
-      install_requires=['zope.deprecation',
-                        'zope.interface',
+      install_requires=['zope_deprecation',
+                        'zope_interface',
                        ],
       include_package_data = True,
 
