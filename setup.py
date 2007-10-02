41d5f2ba41ad48a40f2d0ed406e677c1875c1ad1
Stephan Richter <srichter@gmail.com>
2007-10-02 13:46:48 +0000

    Improve the package meta-data.

--- setup.py
+++ setup.py
@@ -15,29 +15,44 @@
 
 $Id$
 """
-
 import os
-
 from setuptools import setup, find_packages
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
 setup(name='zope.exceptions',
-      version = '3.4.0b2',
-      url='http://svn.zope.org/zope.exceptions',
-      license='ZPL 2.1',
-      description='Zope Exceptions',
+      version = '3.4.0',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
-      long_description=open('README.txt').read(),
-
+      description='Zope Exceptions',
+      long_description=(
+          read('README.txt')
+          + '\n\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = 'zope3 exceptions',
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://cheeseshop.python.org/pypi/zope.exceptions',
+      license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-
-      namespace_packages=['zope',],
-      tests_require = ['zope.testing'],
+      namespace_packages=['zope'],
+      extras_require = dict(
+          test=['zope.testing'],
+          ),
       install_requires=['setuptools',
                         'zope.interface',
                        ],
       include_package_data = True,
-
       zip_safe = False,
       )
