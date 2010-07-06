b79ea94c6f96984bdc228b964acbece3f7266492
Hanno Schlichting <hanno@hannosch.eu>
2010-07-06 17:34:55 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(*rnames):
 
 
 setup(name='zope.exceptions',
-      version = '3.6.1',
+      version = '3.6.2dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Exceptions',
