dc0f35743c83f89c2dc9fdef222270f63779be05
Adam Groszer <agroszer@mail.com>
2012-03-28 13:53:57 +0000

    Preparing release 3.6.2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(*rnames):
 
 
 setup(name='zope.exceptions',
-      version = '3.6.2dev',
+      version='3.6.2',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Exceptions',
