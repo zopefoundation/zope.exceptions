022f61867fdd79231d3d9ebfb524887271455df0
Adam Groszer <agroszer@mail.com>
2012-03-28 13:55:01 +0000

    Back to development: 3.6.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(*rnames):
 
 
 setup(name='zope.exceptions',
-      version='3.6.2',
+      version='3.6.3.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Exceptions',
