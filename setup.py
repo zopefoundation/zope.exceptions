221db53cc4bd5bb8cb25e0fec11a4ee81c7b5103
Tres Seaver <tseaver@palladion.com>
2012-04-06 19:36:10 +0000

    Note supported versions change.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(*rnames):
 
 
 setup(name='zope.exceptions',
-      version='3.7.2.dev0',
+      version='4.0dev',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Exceptions',
