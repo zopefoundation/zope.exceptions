0f265ce39acc9c4ba907871b1e1863bc806d85eb
Adam Groszer <agroszer@gmail.com>
2017-04-12 09:57:53 +0200

    Back to development: 4.1.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.exceptions',
-      version='4.1.0',
+      version='4.1.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Exceptions',
